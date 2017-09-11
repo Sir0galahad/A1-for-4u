b0036be764fffa7c577cdf254a6cf336678f495f
millc9988 <millc9988@HRH-IC0027327.wrdsb.ca>
2017-09-11 09:40:09 -0400

    Just began question 10 on first assignment

---
+++ src/grade/pkg12/a1/A1Q10.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grade.pkg12.a1;
+
+/**
+ *
+ * @author millc9988
+ */
+public class A1Q10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
